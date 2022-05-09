2c15035d27af13209d9d778011d0382278536973
SardorproDev <sardorqobilov555@gmail.com>
2022-05-09 20:49:21 +0500

    New JS prezintatsiya after Qobilov Sardor

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let x = 5;
+let y = 6;
+
+console.log(x+y);
+
+let num = 4 + "radio";
+console.log(typeof(num)); //numberni stringa qo'shsak string chiqadi
+console.log(4 + "radio");
+
+// inkrement va dikrement;
+let z = 10;
+console.log(z++);
+console.log(z); // 11-chiqadi 
+console.log(++z);
+
